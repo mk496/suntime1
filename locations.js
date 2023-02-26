578afe527b939209ad22a86518fa3eaab1027b60
mk496 <35866757+mk496@users.noreply.github.com>
2023-02-26 09:57:10 +0100

    Update locations.js

--- locations.js
+++ locations.js
@@ -3,7 +3,7 @@ var locations = [{"city":"Poznan - freedom city", "latitude":52.408333, "longitu
 {"city":"Moskwa - Facist Capital", "latitude":55.75, "longitude":37.616667,"timeZone": 3},
 {"city":"New York", "latitude":40.43, "longitude":-74.01,"timeZone": -5}, 
 {"city":"Los Angeles", "latitude":34.098907, "longitude":-118.327759,"timeZone": -8},      
-{"city":"Bangalore", "latitude":12.972442, "longitude":77.580643,"timeZone": 5.5},   
+{"city":"Bangalore", "latitude":12.972442, "longitude":77.580643,"timeZone": 5},   
 {"city":"Anchorage", "latitude":61.21, "longitude":-149.88,"timeZone": -9},
 {"city":"Longyearbyen", "latitude":78.21, "longitude":15.33,"timeZone": 1},
 {"city":"McMurdo", "latitude":-77.84, "longitude":166.66,"timeZone": 12},
